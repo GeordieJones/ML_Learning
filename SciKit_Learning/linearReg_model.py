22cc2ad0c87ceaf7e954ceaae8cced4cab8308aa
Geordie Jones <geordiejonesnufc@icloud.com>
2025-07-12 10:52:49 -0400

    adaline classes

--- SciKit_Learning/linearReg_model.py
+++ SciKit_Learning/linearReg_model.py
@@ -45,6 +45,99 @@ class Perception:
         return np.where(self.net_input(x) >= 0.0,1,0)
 
 
+class AdalineGD:
+    def __init__(self, eta=0.01, n_iter = 50, random_state=1):
+        self.eta = eta
+        self.n_iter = n_iter
+        self.random_state = random_state
+    
+    def fit(self, x, y):
+        rgen = np.random.RandomState(self.random_state)
+        self.w_ = rgen.normal(loc=0.0, scale= 0.01, size=x.shape[1])
+        self.b_ = np.float_(0.)
+        self.losses_ = []
+        for i in range(self.n_iter):
+            net_input = self.net_input(x)
+            output = self.activation(net_input)
+            errors = (y - output)
+            self.w_ += self.eta * 2.0 *x.T.dot(errors)/ x.shape[0]
+            self.b_ += self.eta * 2.0 * errors.mean()
+            loss = (errors**2).mean()
+            self.losses_.append(loss)
+        return self
+
+    def net_input(self, x):
+        return np.dot(x, self.w_) + self.b_
+
+    def activation(self,x):
+        return x
+    
+    def predict(self, x):
+        return np.where(self.activation(self.net_input(x)) >= 0.0,1,0)
+
+
+class AdalineSGD:
+    def __init__(self, eta=0.01, n_iter = 10, shuffle=True, random_state=None):
+        self.eta = eta
+        self.n_iter = n_iter
+        self.random_state = random_state
+        self.shuffle = shuffle
+        self.w_initialized = False
+    
+    def fit(self, x, y):
+        self._initialized_weights(x.shape[1])
+        self.losses_ = []
+        for i in range(self.n_iter):
+            if self.shuffle:
+                x,y = self._shuffle(x,y)
+            losses = []
+            for xi, target in zip(x,y):
+                losses.append(self._update_weights(xi, target))
+            avg_loss = np.mean(losses)
+            self.losses_.append(avg_loss)
+        return self
+
+
+    def partial_fit(self, x, y):
+        if not self.w_initialized:
+            self._initialize_weights(x.shape[1])
+        if y.ravel().shape[0] > 1:
+            for xi, target in zip(x,y):
+                self._update_weights(xi,target)
+        else:
+            self.__update_weights(x,y)
+        return self
+    
+    def shuffle(self, x, y):
+        r = self.rgen.permutation(len(y))
+        return x[r], y[r]
+    
+    def _initialize_weights(self,m):
+        self.rgen = np.random.RandomState(self.random_state)
+        self.w_ = self.rgen.normal(loc = 0.0, scale = 0.01, size = m)
+        self.b_ = np.float_(0.)
+        self.w_initialized = True
+
+    def update_weights(self, xi, target):
+        output = self.activation(self.net_input(xi))
+        error = (target - output)
+        self.w_+= self.eta*2.0*xi*(error)
+        self.b += self.eta * 2.0 * error
+        loss = error**2
+        return loss
+
+    def net_input(self, x):
+        return np.dot(x, self.w_) + self.b_
+
+    def activation(self,x):
+        return x
+    
+    def predict(self, x):
+        return np.where(self.activation(self.net_input(x)) >= 0.0,1,0)
+
+
+
+
 
 s = 'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
 
@@ -94,8 +187,45 @@ def plot_decision_regions(X,y,classifier,resolution=0.02):
                     label = f'Class {cl}',
                     edgecolor = 'black')
 
-plot_decision_regions(x,y, classifier=ppn)
+'''plot_decision_regions(x,y, classifier=ppn)
 plt.xlabel('Sepel length [cm]')
 plt.ylabel('petal length [cm]')
 plt.legend(loc = 'upper left')
+plt.show()
+
+fig, ax = plt.subplots(nrows= 1, ncols = 2, figsize=(10,4))
+ada1 = AdalineGD(n_iter=15, eta = 0.1).fit(x,y)
+ax[0].plot(range(1,len(ada1.losses_)+1),
+            np.log10(ada1.losses_), marker='o')
+ax[0].set_xlabel('Epochs')
+ax[0].set_ylabel('log(mean squared error)')
+ax[0].set_title('adaline - learning rate 0.1')
+
+ada2 = AdalineGD(n_iter=15, eta = 0.0001).fit(x,y)
+ax[1].plot(range(1,len(ada2.losses_)+1),
+            np.log10(ada2.losses_), marker='o')
+ax[1].set_xlabel('Epochs')
+ax[1].set_ylabel('log(mean squared error)')
+ax[1].set_title('adaline - learning rate 0.0001')
+plt.show()'''
+
+X_std = np.copy(x)
+X_std[:,0] = (x[:,0] - x[:,0].mean())/x[:,0].std()
+X_std[:,1] = (x[:,1] - x[:,1].mean())/x[:,1].std()
+
+ada_gd = AdalineGD(n_iter=20, eta=0.5)
+ada_gd.fit(X_std, y)
+
+'''plot_decision_regions(X_std, y, classifier=ada_gd)
+plt.xlabel('Septal length')
+plt.ylabel('petal length')
+plt.title('adaline - gradient decent')
+plt.legend(loc = 'upper left')
+plt.tight_layout()
+plt.show()'''
+
+plt.plot(range(1,len(ada_gd.losses_)+1), ada_gd.losses_,marker='o')
+plt.xlabel('epochs')
+plt.ylabel('mean squared error')
+plt.tight_layout()
 plt.show()
